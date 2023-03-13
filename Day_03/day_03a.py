55a5c9f7e5a985e4bff641475a86bdd4c1349eb1
GregGamer <gregamer98@gmail.com>
2023-03-13 21:37:53 +0100

    funzt

--- Day_03/day_03a.py
+++ Day_03/day_03a.py
@@ -1,3 +1,29 @@
 # Challenge Link: https://adventofcode.com/2015/day/3
 
-   
+filename = "Day_03/Jinput.in"
+with open(filename) as f:
+    data = f.read()
+
+x,y = 0,0
+#     y+1
+# x-1     x+1
+#     y-1
+route = set()
+for d in data:
+    index = 'x'+str(x)+'y'+str(y)
+    route.add(index)
+
+    if d == "^":
+        y+=1
+    if d == ">":
+        x+=1
+    if d == "v":
+        y-=1
+    if d == "<":
+        x-=1
+
+index = 'x'+str(x)+'y'+str(y)
+route.add(index)
+
+# print(route)
+print(len(route))
